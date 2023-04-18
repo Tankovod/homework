a45ed939cbaba8a0ea1f3ff90ca5203ad1968f5b
Tankovod <Wotdimas@gmail.com>
2023-04-18 11:44:49 +0300

    inintial commit

--- 3_4.py
+++ 3_4.py
@@ -1,14 +1,12 @@
 # ------------- LESSON 3, EXERCISE 3 -------------------
 # User enters 3 numbers, display the counts of positive and negative values
 
-positive = 0
-negative = 0
-numbers_list = []
+positive, negative = 0, 0
 
 # ----- 1st way ---------
-numbers_list.append(input('enter the first number>').strip())
-numbers_list.append(input('enter the second number>').strip())
-numbers_list.append(input('enter the third number>').strip())
+numbers_list = [input('enter the first number>').strip(),
+                input('enter the second number>').strip(),
+                input('enter the third number>').strip()]
 
 for number in numbers_list:
     if number.startswith('-') and number[1:].isdigit():
@@ -22,21 +20,22 @@ print(f'There are {positive} positive and {negative} negative values.')
 # ----- 1st way ---------
 
 # ------- 2nd way -------
-# try:
-#     numbers_list.append(int(input('enter the first number>')))
-#     numbers_list.append(int(input('enter the second number>')))
-#     numbers_list.append(int(input('enter the third number>')))
-#
-#     for number in numbers_list:
-#         if number < 0:
-#             negative += 1
-#         else:
-#             positive += 1
-#
-#     print(f'There are {positive} positive and {negative} negative values.')
-#
-# except Exception as ex_:
-#     print(ex_)
-#     print('!__check your input data__!')
-# ------- 2nd way -------
+positive, negative = 0, 0
+try:
 
+    numbers_list = [int(input('enter the first number>')),
+                    int(input('enter the second number>')),
+                    int(input('enter the third number>'))]
+
+    for number in numbers_list:
+        if number < 0:
+            negative += 1
+        else:
+            positive += 1
+
+    print(f'There are {positive} positive and {negative} negative values.')
+
+except Exception as ex_:
+    print(ex_)
+    print('!__check your input data__!')
+# ------- 2nd way -------
