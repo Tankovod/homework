5dc0ecb2181b4fd9d242eb0bfac3d34dabc580e4
Tankovod <Wotdimas@gmail.com>
2023-04-18 09:49:04 +0300

    inintial commit

--- 3_1.py
+++ 3_1.py
@@ -1,2 +1,12 @@
 # ------------- LESSON 3, EXERCISE 1 -------------------
+# User enter the statement, replace all gaps with '-' symbols by 2 different ways
+
+input_text = input('enter any text>')
+
+# 1st way:
+print(input_text.replace(' ', '-'))
+
+# 2nd way:
+split_text = input_text.split(' ')
+print('-'.join(split_text))
 
