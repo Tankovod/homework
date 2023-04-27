ca139d79d01552425ec0c144ab948d64b432201f
Tankovod <Wotdimas@gmail.com>
2023-04-27 20:47:08 +0300

    simple commit

--- 5_6.py
+++ 5_6.py
@@ -1,5 +1,5 @@
 # ------------- LESSON 5, EXERCISE 6 -------------------
-# Guess the number
+# <<<Guess the number>>>
 # You need to write the game "Guess the number" and
 # show how many times user attempt to guess it
 
@@ -10,7 +10,10 @@ a = randint(1, 100)
 
 attempts, guess = 0, None
 while guess != a:
-    guess = int(input('Guess the number !!:D >'))
-    attempts += 1
+    try:
+        guess = int(input('Guess the number !!:D >'))
+        attempts += 1
+    except ValueError:
+        print('Try any whole number, please ...')
 
 print(f'Congratulations !!! You had to guess {attempts} times ! XD')
