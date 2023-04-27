1422ea600de9ae78afaca777990bad002668747d
Tankovod <Wotdimas@gmail.com>
2023-04-27 23:05:17 +0300

    simple commit

---
+++ 5_7.py
@@ -0,0 +1,56 @@
+# ------------- LESSON 5, EXERCISE 7 -------------------
+# In witcher's world there are coins of denomination 1, 5, 10, 25
+# Write the program that defines minimal amount of coins to pay to the witcher
+
+sum_of_gold = int(input('How much gold should be given to the witcher?> '))
+
+# ----- 1st way -----
+extra_coins = sum_of_gold % 25
+coin25 = (sum_of_gold - extra_coins) / 25
+coin10 = (extra_coins - extra_coins % 10) / 10
+extra_coins = extra_coins - coin10 * 10
+coin5 = (extra_coins - extra_coins % 5) / 5
+extra_coins = extra_coins - coin5 * 5
+coin1 = (extra_coins - extra_coins % 1) / 1
+
+print(f'~~~ You need {int(coin25)} coins with value 25, {int(coin10)} with value 10,'
+      f' {int(coin5)} with value 5 and {int(coin1)} with value 1.~~~')
+
+
+# ----- 1st way -----
+
+# ----- 2nd way -----
+def coin_plus(coins, all_gold, coin_number):
+    coin_count, result_coins = 0, coins
+    while coin_number and coin_number * (coin_count + 1) <= all_gold - coins:
+        result_coins += coin_number
+        coin_count += 1
+    return result_coins, coin_count
+
+
+final_coins = 0
+
+final_coins, coin25 = coin_plus(final_coins, sum_of_gold, 25)
+final_coins, coin10 = coin_plus(final_coins, sum_of_gold, 10)
+final_coins, coin5 = coin_plus(final_coins, sum_of_gold, 5)
+final_coins, coin1 = coin_plus(final_coins, sum_of_gold, 1)
+
+print(f'~~~ You need {coin25} coins with value 25, {coin10} with value 10,'
+      f' {coin5} with value 5 and {coin1} with value 1.~~~')
+# ----- 2nd way -----
+
+# ----- 3rd way -----
+coin1, coin5, coin10, coin25 = 0, 0, 0, 0
+
+while 25 * (coin25 + 1) < sum_of_gold:
+    coin25 += 1
+while 10 * (coin10 + 1) < sum_of_gold - coin25 * 25:
+    coin10 += 1
+while 5 * (coin5 + 1) < sum_of_gold - coin25 * 25 - coin10 * 10:
+    coin5 += 1
+while 1 and coin1 < sum_of_gold - coin25 * 25 - coin10 * 10 - coin5 * 5:
+    coin1 += 1
+
+print(f'~~~ You need {coin25} coins with value 25, {coin10} with value 10,'
+      f' {coin5} with value 5 and {coin1} with value 1.~~~')
+# ----- 3rd way -----
