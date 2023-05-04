548d76bde0cafab68f19b3b20fe019320978b53c
Tankovod <Wotdimas@gmail.com>
2023-05-04 17:45:37 +0300

    simple commit

--- 6_4.py
+++ 6_4.py
@@ -1,11 +1,19 @@
 # ------------- LESSON 6, EXERCISE 4 -------------------
-# filter list for string types
+# Filter list for string types
 
-def filter_for_string(list_to_filter, lngth):
-    i = 0
+# ---- 1st way ----
+def filter_for_string(list_to_filter):
+    for i in list_to_filter:
+        if isinstance(i, list):
+            list_to_filter[list_to_filter.index(i)] = filter_for_string(i)
+    return [*filter(lambda x: isinstance(x, (str, list)), list_to_filter)]
+# ---- 1st way ----
+# ---- 2nd way ----
+def filter_for_string2(list_to_filter, lngth):
+    i = 0  # counter
     while i != lngth:
         if isinstance(list_to_filter[i], list):
-            any_in_list = filter_for_string(list_to_filter[i], len(list_to_filter[i]))
+            any_in_list = filter_for_string2(list_to_filter[i], len(list_to_filter[i]))
             if not any_in_list:  # if blank list returned
                 list_to_filter.remove(list_to_filter[i])  # remove blank list
                 lngth -= 1
@@ -18,10 +26,11 @@ def filter_for_string(list_to_filter, lngth):
             i += 1
 
     return list_to_filter
+# ---- 2nd way ----
 
+list_for_filter = [1, 2, 3, [4, '234', 45, 345, '34', [234, 0, '34', '90'], [234, '34']],
+                   'efg', '234', None, 234, 234, True, {'key': 'value'}, 'rsgh', ['345', '00000', '3454', 345, 345],
+                   '325', [34, 34, [34, 34, '7'], [1, 2]]]
 
-list_for_filter = [1, 2, 3, [4, '234', 45, 345, '34', [234, 0, '34', '90'], [234,'34']],
-                   'efg', '234', None, 234, 234, True, {'key': 'value'}, 'rsgh', ['345','00000','3454', 345, 345],
-                   '325', [34, 34, [ 34, 34,'7']]]
-
-print(filter_for_string(list_for_filter, len(list_for_filter)))
+print(filter_for_string2(list_for_filter, len(list_for_filter)))
+print(filter_for_string(list_for_filter))
---
+++ 6_5.py
@@ -0,0 +1,16 @@
+# ------------- LESSON 6, EXERCISE 5 -------------------
+# Reverse list of integers without using of reverse(), slice and additional list
+
+list_to_reverse = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 33, 44, 100, 101, 10001, 345345, 0, 0, 4, 5, 6, 8, 9]
+
+def reverse_the_list(lst):
+    last = len(lst)
+    for i in range(len(lst), 0, -1):
+        lst.append(lst[i - 1])
+    for i in range(last):
+        lst.pop(0)
+    return lst
+
+    # return [*map(lambda x: int(x), [*filter(lambda x: isinstance(x, str), lst)])]
+
+print(reverse_the_list(list_to_reverse))
