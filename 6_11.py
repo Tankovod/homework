4e5a4bd844fd70f2e448eaf132d8d7fe73a973d3
Tankovod <Wotdimas@gmail.com>
2023-05-08 10:43:07 +0300

    simple commit

--- 6_11.py
+++ 6_11.py
@@ -3,12 +3,18 @@
 from math import sqrt
 
 def generate_simple_nums(count):
-    number = 1
+    number = 2
     for elem in range(1, count + 1):
-        if number != 1:
-            while any([number for i in range(2, number // 2 + 1) if number % i == 0]) and number not in [2, 3, 5, 7]:
-                number += 1
-            yield number
+        if number not in [2, 3, 5, 7]:
+            tim = 0
+            while tim == 0:
+                tim = 1
+                for divider in range(2, number // 2 + 1):
+                    if number % divider == 0:
+                        number += 1
+                        tim = 0
+                        break
+        yield number
         number += 1
 
 
