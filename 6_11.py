6443959dea7631f08d69feb8d300cffb0e7c9e23
Tankovod <Wotdimas@gmail.com>
2023-05-08 09:58:32 +0300

    simple commit

--- 6_11.py
+++ 6_11.py
@@ -1,15 +1,13 @@
 # ------------- LESSON 6, EXERCISE 11 -------------------
 # Write function generator, received int count and return count amount simple numbers
-
+from math import sqrt
 
 def generate_simple_nums(count):
     number = 1
-    simple_numbs = []
     for elem in range(1, count + 1):
         if number != 1:
-            while any([number for i in simple_numbs if number % i == 0]) and number not in [2, 3, 5, 7]:
+            while any([number for i in range(2, number // 2 + 1) if number % i == 0]) and number not in [2, 3, 5, 7]:
                 number += 1
-            simple_numbs.append(number)
             yield number
         number += 1
 
